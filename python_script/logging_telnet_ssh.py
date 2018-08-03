16a4aa15df5bf0e43699acfe5a79cee92f8c4764
mokomoko511 <reriuria@gmail.com>
2018-08-04 03:14:19 +0900

    merge telnet and ssh.

--- python_script/logging_telnet_ssh.py
+++ python_script/logging_telnet_ssh.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import sys
-import telnetlib
-import socket
-import datetime
-import argparse
-import random
-import codecs
+import sys, argparse, re, random, codecs
+import telnetlib, socket
+import datetime, time
+import paramiko
 from time import sleep
+
 
 def telnet_main(ipaddr, user, passwd, cmdlist, logfolder, logfilename):
 
@@ -36,7 +34,6 @@ def telnet_main(ipaddr, user, passwd, cmdlist, logfolder, logfilename):
         #Get hostname
         tn.write(("\n").encode('ascii'))
         tn.read_until(b"\n")
-
         wait_hostname=tn.read_until(b"#")
         #print(wait_hostname)
         hostname_get=(wait_hostname.strip(b"#").decode('ascii'))
@@ -98,6 +95,80 @@ def telnet_main(ipaddr, user, passwd, cmdlist, logfolder, logfilename):
     fp.write(ret2)
     fp.close()
 
+
+def ssh_main(ipaddr, user, passwd, cmdlist, logfolder, logfilename):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(ipaddr, username=user, password=passwd)
+    except:
+        print("Connetion timeout. Please check following IPAddress : " + ipaddr)
+        input("Please input any key...")
+        sys.exit()
+
+    print(ipaddr + " connect to ssh successful. Start logging..." + "\n")
+
+    #Login prompt and getting hostname. This method is invoke in shell.
+    channel = ssh.invoke_shell()
+    while not channel.recv_ready():
+        time.sleep(1)
+    results = channel.recv(2048)
+    channel.send('enable\n')
+    while not channel.recv_ready():
+        time.sleep(1)
+    results += channel.recv(2048)
+    channel.send('\n')
+    while not channel.recv_ready():
+        time.sleep(1)
+    results2 = channel.recv(2048)
+    r = re.compile(r'\r')
+    ret = re.sub(r,'',results.decode('utf-8'))
+    host_temp = re.sub('\r\r\n','',results2.decode('utf-8'))
+    hostname_get=(host_temp.strip("#"))
+
+    #Import cmdlists
+    cmds = []
+    for line in open(cmdlist, 'r').readlines():
+        cmds.append(line.strip())
+
+    #Input command list.
+    try:
+        for item in cmds:
+            stdin, stdout, stderr = ssh.exec_command(item)
+            ret+=host_temp + item + '\n'
+            print('excute command : ' +  item)
+            for line in stdout:
+                ret+=line
+                #print(ret)
+    except:
+        print("Something went wrong. Please retry host to " + ipaddr)
+        input("Please input any key...")
+        sys.exit()
+
+    print("\n" + "Completed get log. Close connection...")
+    ssh.close()
+
+    #writing result to log file
+    if logfilename is not None:
+        if logfolder is not None:
+            print(logfilename)
+            filename=logfilename
+        else:
+            print(logfolder + logfilename)
+            filename="%s%s" % (logfolder,logfilename)
+    elif logfolder is not None:
+        print(logfolder)
+        now = datetime.datetime.now()
+        filename = "%s%s_%i%.2i%.2i_%.2i%.2i%.2i.txt" % (logfolder,hostname_get,now.year,now.month,now.day,now.hour,now.minute,now.second)
+    else:
+        now = datetime.datetime.now()
+        filename = "%s_%i%.2i%.2i_%.2i%.2i%.2i.txt" % (hostname_get,now.year,now.month,now.day,now.hour,now.minute,now.second)
+
+    #If the log string was not ascii, that log line ignore.
+    fp=open(filename,"w")
+    fp.write(ret)
+    fp.close()
+
 def get_args():
     #help ArgumentParser
     parser = argparse.ArgumentParser()
@@ -107,6 +178,9 @@ def get_args():
         parser.add_argument("password", help="set password", type=str)
         parser.add_argument("cmdlist", help="set command line list file",type=str)
 
+    #Select connection method, Default is telnet.
+    parser.add_argument('-t', '--telnet', help='Set telnet connection method, telnet is default.',action='store_const', const=True, default=True)
+    parser.add_argument('-s', '--ssh', help='Set ssh connection method',action='store_const', const=True, default=False)
     parser.add_argument('-f', '--logfolder', help='Set the log folder',  type=str)
     parser.add_argument('-l', '--logfilename', help='Default logfile name "<hostname>_<date>", you can defined the logfilename',  type=str)
 
@@ -123,7 +197,10 @@ def main():
     logfolder=args.logfolder
     logfilename=args.logfilename
 
-    telnet_main(ipaddr, user, passwd, cmdlist, logfolder, logfilename)
+    if args.ssh == True:
+        ssh_main(ipaddr, user, passwd, cmdlist, logfolder, logfilename)
+    else:
+        telnet_main(ipaddr, user, passwd, cmdlist, logfolder, logfilename)
 
 if __name__ == '__main__':
     main()
